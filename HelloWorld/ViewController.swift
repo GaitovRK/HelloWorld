21b562e0e80b7c3bbc9d580979d14e98c4560a1f
GaitovRK <rashid.gaitov@gmail.com>
2022-03-11 14:20:21 +0600

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Rashid Gaitov on 11.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
